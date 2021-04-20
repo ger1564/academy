33c4881774bf0b9aa246c2b9db2a135d35bf46ca
Ger <ing.lugomg@gmail.com>
2021-04-19 22:50:27 -0500

    Alumno

--- models/alumno.py
+++ models/alumno.py
@@ -9,8 +9,8 @@ class Alumno(models.Model):
     name = fields.Char(string='Nombre', required=True)
     num_cuenta = fields.Integer(string='Cuenta', required=True)
     correo = fields.Char(string='correo', required=True)
+    active = fields.Boolean(string='Active', default=True)
     carrera = fields.Selection(string='Carrera', 
                             selection=[('computacion','Computacio'),
                                     ('mecanica','Mecanica'),
-                                    ('telecom','Telecomunicaciones')],
-    active = fields.Boolean(string='Active', default=True)
+                                    ('telecom','Telecomunicaciones')]
