7271ed247f7107918d08bbe50b55a8de2507cfb5
Ger <ing.lugomg@gmail.com>
2021-04-19 21:53:07 -0500

    m

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{    
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage Trainning""",
+    'description': """
+                    Academy Module to manage Trainning:
+                    -Courses
+                    -Sessions
+                    -Attendees
+    """,
+    'author':'Odoo',
+    'website':'https://www.odoo.com',
+    'category': 'Trainning',
+    'version':'0.1',
+    'depends': ['base'],
+    'data': [
+
+    
+    ],
+    'demo': [
+
+    ],
+    
+}
